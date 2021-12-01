3e3633e8f3782ea03f3f315cfae650343160da11
PeatYT <89882479+PeatYT@users.noreply.github.com>
2021-12-01 13:32:34 -0600

    added previous code and new sweeper/arm stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/P.java
@@ -1,4 +1,90 @@
 package org.firstinspires.ftc.teamcode;
 
-public class P {
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import android.graphics.Point;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+@TeleOp(name="peat test", group="Linear Opmode")
+public class P extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    public DcMotor FR;
+    public DcMotor FL;
+    public DcMotor BR;
+    public DcMotor BL;
+
+    public DcMotor arm;
+    public Servo armEnd;
+
+    public DcMotor sweeper;
+
+    public long pt;
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //
+//        R = hardwareMap.get(DcMotor.class, "R");
+//        L = hardwareMap.get(DcMotor.class, "L");
+//
+//        RS = hardwareMap.get(Servo.class, "RS");
+//        LS = hardwareMap.get(Servo.class, "LS");
+//        //servo.setPosition(0);
+//
+//        p=new Point(0,0);
+
+        // runs the moment  is initialized
+        waitForStart();
+        runtime.reset();
+
+        armEnd.resetDeviceConfigurationForOpMode();
+        // runs after driver presses play
+        while (opModeIsActive()) {
+            motors();
+
+            if (gamepad1.dpad_right){arm.setTargetPosition(arm.getTargetPosition()+1);}
+            if (gamepad1.dpad_left){arm.setTargetPosition(arm.getTargetPosition()-1);}
+            if (gamepad1.dpad_down){armEnd.setPosition(-180);}
+            if (gamepad1.dpad_up){armEnd.setPosition(0);}
+
+            if(gamepad1.a){sweeper.setPower(-1);}else{sweeper.setPower(0);}
+
+            telemetry.addLine(
+                    "UpS (ms)"+  1000/((double)(System.nanoTime()-pt)/1000000)+"\n"+
+                              "motor position:" + "\n"+
+                            "FR: "+FR.getCurrentPosition()+"\n" +
+                            "FL: "+FL.getCurrentPosition()+"\n" +
+                            "BR: "+BR.getCurrentPosition()+"\n" +
+                            "BL: "+BL.getCurrentPosition()+"\n" +
+                            "arm "+arm.getCurrentPosition()+"\n"+
+                            "servo "+armEnd.getPosition()+"\n"+
+                            "sweeper "+ sweeper.getCurrentPosition()+"\n"
+            );
+            pt=System.nanoTime();
+            telemetry.update();
+        }
+    }
+
+    void motors() {
+        //complex trial-and-error stuff
+        FR.setPower(Range.clip(-gamepad1.left_stick_y-gamepad1.left_stick_x-.75*gamepad1.right_stick_x,-1,1));
+        FL.setPower(Range.clip(-gamepad1.left_stick_y+gamepad1.left_stick_x+.75*gamepad1.right_stick_x,-1,1));
+        BR.setPower(Range.clip(-gamepad1.left_stick_y+gamepad1.left_stick_x-.75*gamepad1.right_stick_x,-1,1));
+        BL.setPower(Range.clip(-gamepad1.left_stick_y-gamepad1.left_stick_x+.75*gamepad1.right_stick_x,-1,1));
+    }
 }
