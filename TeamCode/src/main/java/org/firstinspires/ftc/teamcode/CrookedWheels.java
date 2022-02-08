4550ecb774430d0530197a04cc091486dac29e9f
bpomara <mom2gramps4mom>
2022-02-07 19:35:33 -0600

    CrookedCommit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrookedWheels.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Crooked Wheels", group="Autonomous")
+public class CrookedWheels extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    CrookedBot robot;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot = new CrookedBot(hardwareMap);
+
+        // runs the moment robot is initialized
+        waitForStart();
+        runtime.reset();
+
+
+        // runs after driver presses play
+        while (opModeIsActive()) {
+            double vertical = -gamepad1.left_stick_y;
+            double horizontal = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+
+            robot.FL.setPower(Range.clip(vertical + horizontal + turn, -1, 1));
+            robot.FR.setPower(Range.clip(vertical - horizontal - turn, -1, 1));
+            robot.BL.setPower(Range.clip(vertical - horizontal + turn, -1, 1));
+            robot.BR.setPower(Range.clip(vertical + horizontal - turn, -1, 1));
+
+            telemetry.addData("Motors", "FL (%.2f), FR (%.2f), BL (%.2f), BR (%.2f)", robot.FL.getPower(), robot.FR.getPower(), robot.BL.getPower(), robot.BR.getPower());
+            telemetry.addData("Encoders", "FL (%d), FR (%d), BL (%d), BR (%d)", robot.FL.getCurrentPosition(), robot.FR.getCurrentPosition(), robot.BL.getCurrentPosition(), robot.BR.getCurrentPosition());
+
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPortFinder.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Motor Port Finder", group="Autonomous")
+public class MotorPortFinder extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    RobotHardware robot;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot = new RobotHardware(hardwareMap);
+
+        // runs the moment robot is initialized
+        waitForStart();
+        runtime.reset();
+
+        // runs after driver presses play
+        while (opModeIsActive()) {
+
+            robot.FR.setPower(0);
+            robot.FL.setPower(0);
+            robot.BR.setPower(0);
+            robot.BL.setPower(0);
+            if(gamepad1.dpad_up){
+                robot.FR.setPower(1);
+                telemetry.addData("Motor Type", "Front Right");
+            }else if(gamepad1.dpad_left){
+                robot.FL.setPower(1);
+                telemetry.addData("Motor Type", "Front Left");
+            }else if(gamepad1.dpad_down){
+                robot.BL.setPower(1);
+                telemetry.addData("Motor Type", "Back Left");
+            }else if(gamepad1.dpad_right){
+                robot.BR.setPower(1);
+                telemetry.addData("Motor Type", "Back Right");
+            }
+
+            telemetry.update();
+        }
+    }
+}
